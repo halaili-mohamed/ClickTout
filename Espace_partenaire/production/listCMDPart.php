91180530f5e600517c84197a16108f21b0fd94ef
wafa raboudi <wafaraboudi10@gmail.com>
2019-09-12 14:13:44 +0200

    extensionPhp

---
+++ Espace_partenaire/production/listCMDPart.php
@@ -0,0 +1,395 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!-- Meta, title, CSS, favicons, etc. -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="icon" href="images/icon.png" type="image/ico" />
+    <title>Espace partenaire </title>
+
+    <!-- Bootstrap -->
+    <link href="../vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link href="../vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <!-- NProgress -->
+    <link href="../vendors/nprogress/nprogress.css" rel="stylesheet">
+    <!-- iCheck -->
+    <link href="../vendors/iCheck/skins/flat/green.css" rel="stylesheet">
+
+    <!-- Custom Theme Style -->
+    <link href="../build/css/custom.min.css" rel="stylesheet">
+  </head>
+
+  <body class="nav-md">
+    <div class="container body">
+      <div class="main_container">
+        <div class="col-md-3 left_col">
+          <div class="left_col scroll-view">
+            <div class="navbar nav_title" style="border: 0;">
+              <a href="index.html" class="site_title"><i class="fa fa-cube"></i> <span>Click TOUT</span></a>
+            </div>
+
+            <div class="clearfix"></div>
+
+            <!-- menu profile quick info -->
+            <div class="profile clearfix">
+              
+             
+            </div>
+            <!-- /menu profile quick info -->
+
+            <br />
+
+            <!-- sidebar menu -->
+            <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+              <div class="menu_section">
+                
+                <ul class="nav side-menu">
+                  <li><a href="profilePart.html"><i class="fa fa-user"></i> Mon compte </a>
+                    
+                 
+                       <li><a href="formCmdPart.html"> <i class="fa fa-edit"></i>Réservation</a></li>
+                      <li><a href="listCMDPart.html"><i class="fa fa-list"></i>Liste en attente</a></li>
+					  <li><a href="historiquePart.html"><i class="fa fa-clock-o"></i>Historique</a></li>
+                    
+                  
+				  <li><a href="reclamationPart.html"><i class="fa fa-comments-o"></i> Réclamation </a>
+                    
+                  </li>
+                    
+                 
+                </ul>
+              </div>
+              
+
+            </div>
+            <!-- /sidebar menu -->
+
+            
+          </div>
+        </div>
+
+        <!-- top navigation -->
+        <div class="top_nav">
+          <div class="nav_menu">
+            <nav>
+              <div class="nav toggle">
+                <a id="menu_toggle"><i class="fa fa-bars"></i></a>
+              </div>
+
+			  
+              <ul class="nav navbar-nav navbar-right">
+                <li class="">
+                  <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                    <img src="images/.jpg" alt="">yser name
+                    <span class=" fa fa-angle-down"></span>
+                  </a>
+                  <ul class="dropdown-menu dropdown-usermenu pull-right">
+                    <li><a href="javascript:;"> Mon compte</a></li>
+                   
+                    <li><a href="login.html"><i class="fa fa-sign-out pull-right"></i> Déconnexion</a></li>
+                  </ul>
+                </li>
+
+               
+              </ul>
+            </nav>
+          </div>
+        </div>
+        <!-- /top navigation -->
+
+        <!-- page content -->
+        <div class="right_col" role="main">
+          <div class="">
+            <div class="page-title">
+              <div class="title_left">
+			  <div class="col-md-5" align="center">
+                              <img src="images/logo_ctt_final1.jpg" alt="" class="img-circle img-responsive">
+                            </div> 
+                
+              </div>
+
+              <div class="title_right">
+                <div class="col-md-5 col-sm-5 col-xs-12 form-group pull-right top_search">
+                  <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Rechercher...">
+                    <span class="input-group-btn">
+                      <button class="btn btn-default" type="button"><i class="fa fa-search"></i></button>
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            
+            <div class="clearfix"></div>
+
+            <div class="row">
+              <div class="col-md-12">
+                <div class="x_panel">
+                  <div class="x_title">
+				  <div class="col-md-5" align="center">
+                              <img src="images/logo.png" alt="" class="img-circle img-responsive">
+                            </div> 
+                    					<h2>Liste en attente </h2>
+                    <ul class="nav navbar-right panel_toolbox">
+
+                <li class="dropdown">
+                       <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"> Trier par  <i class="fa fa-sort"></i></a>
+                        <ul class="dropdown-menu" role="menu">
+                          <li><a href="#"><i class="fa fa-sort-amount-desc"></i> La plus récente </a>
+                          </li>
+                          <li><a href="#"> <i class="fa fa-sort-amount-asc"></i> La plus ancienne </a>
+                          </li>
+                        </ul>
+                      </li>
+
+               
+             
+                      
+                    </ul>
+                    <div class="clearfix"></div>
+                  </div>
+
+                  <div class="x_content">
+
+                   
+
+                      
+                      <br />
+
+                      <!-- here -->
+
+                      <div class="row">
+              <div class="col-md-12">
+                <div class="x_panel">
+                  <div class="x_content">
+                    <div class="row">
+                      <div class="col-md-12 col-sm-12 col-xs-12 text-center">
+                        <ul class="pagination pagination-split">
+                          <li><a href="#">A</a></li>
+                          <li><a href="#">B</a></li>
+                          <li><a href="#">C</a></li>
+                          <li><a href="#">D</a></li>
+                          <li><a href="#">E</a></li>
+                          <li>...</li>
+                          <li><a href="#">W</a></li>
+                          <li><a href="#">X</a></li>
+                          <li><a href="#">Y</a></li>
+                          <li><a href="#">Z</a></li>
+                        </ul>
+                      </div>
+
+                      <div class="clearfix"></div>
+
+                      <div class="col-md-4 col-sm-4 col-xs-12 profile_details">
+                        <div class="well profile_view">
+                          <div class="col-sm-12">
+                            <h4 class="brief"><i>N° commande: 231</i></h4>
+                            <div class="left col-xs-7">
+                            
+                              <ul class="list-unstyled">
+                                <li>
+							    <i class="fa fa-calendar "></i>  Date:  24 mai  </li> 
+							    <li><i class="fa fa-clock-o "></i>  Horraire:  10H  </li>
+								
+								<li><i class="fa fa-map-marker"></i> Départ: San Francisco </li>
+								
+								<li><i class="fa fa-flag"></i> Destination: San Francisco</li>
+								<li><i class="fa fa-user"></i> Client: Samuel Doe </br></li>
+								<li><i class="fa fa-mobile-phone "></i> Téléphone: 21331A2312 </li>
+                              </ul>
+                            </div>
+                            
+                          </div>
+                          <div class="col-xs-12 bottom text-center" >
+						  <div class="col-xs-12 col-sm-6 emphasis"></div>    
+                            <div class="col-xs-12 col-sm-6 emphasis" >
+                             
+                              <button type="button" class="btn btn-danger btn-xs" >
+                                <i class="fa fa-times"> </i> Annuler 
+                              </button>
+							  <button type="button" class="btn btn-danger btn-xs" >
+                                <i class="fa fa-times"> </i> Annuler 
+                              </button>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+
+                      <div class="col-md-4 col-sm-4 col-xs-12 profile_details">
+                        <div class="well profile_view">
+                          <div class="col-sm-12">
+                            <h4 class="brief"><i>N° commande: 231</i></h4>
+                            <div class="left col-xs-7">
+                            
+                              <ul class="list-unstyled">
+                                <li>
+							    <i class="fa fa-calendar "></i>  Date:  24 mai  </li> 
+							    <li><i class="fa fa-clock-o "></i>  Horraire:  10H  </li>
+								
+								<li><i class="fa fa-map-marker"></i> Départ: San Francisco </li>
+								
+								<li><i class="fa fa-flag"></i> Destination: San Francisco</li>
+								<li><i class="fa fa-user"></i> Client: Samuel Doe </br></li>
+								<li><i class="fa fa-mobile-phone user-profile-icon"></i> Téléphone: 21331A2312 </li>
+                              </ul>
+                            </div>
+                            
+                          </div>
+                          <div class="col-xs-12 bottom text-center" >
+						  <div class="col-xs-12 col-sm-6 emphasis"></div>    
+                            <div class="col-xs-12 col-sm-6 emphasis" >
+                             
+                              <button type="button" class="btn btn-danger btn-xs" >
+                                <i class="fa fa-times"> </i> Annuler 
+                              </button>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+
+                      <div class="col-md-4 col-sm-4 col-xs-12 profile_details">
+                        <div class="well profile_view">
+                          <div class="col-sm-12">
+                            <h4 class="brief"><i>N° commande: 231</i></h4>
+                            <div class="left col-xs-7">
+                            
+                              <ul class="list-unstyled">
+                                <li>
+							    <i class="fa fa-calendar "></i>  Date:  24 mai  </li> 
+							    <li><i class="fa fa-clock-o "></i>  Horraire:  10H  </li>
+								
+								<li><i class="fa fa-map-marker"></i> Départ: San Francisco </li>
+								
+								<li><i class="fa fa-flag"></i> Destination: San Francisco</li>
+								<li><i class="fa fa-user"></i> Client: Samuel Doe </br></li>
+								<li><i class="fa fa-mobile-phone user-profile-icon"></i> Téléphone: 21331A2312 </li>
+                              </ul>
+                            </div>
+                            
+                          </div>
+                          <div class="col-xs-12 bottom text-center" >
+						  <div class="col-xs-12 col-sm-6 emphasis"></div>    
+                            <div class="col-xs-12 col-sm-6 emphasis" >
+                             
+                              <button type="button" class="btn btn-danger btn-xs" >
+                                <i class="fa fa-times"> </i> Annuler 
+                              </button>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+
+                     <div class="col-md-4 col-sm-4 col-xs-12 profile_details">
+                        <div class="well profile_view">
+                          <div class="col-sm-12">
+                            <h4 class="brief"><i>N° commande: 231</i></h4>
+                            <div class="left col-xs-7">
+                            
+                              <ul class="list-unstyled">
+                                <li>
+							    <i class="fa fa-calendar "></i>  Date:  24 mai  </li> 
+							    <li><i class="fa fa-clock-o "></i>  Horraire:  10H  </li>
+								
+								<li><i class="fa fa-map-marker"></i> Départ: San Francisco </li>
+								
+								<li><i class="fa fa-flag"></i> Destination: San Francisco</li>
+								<li><i class="fa fa-user"></i> Client: Samuel Doe </br></li>
+								<li><i class="fa fa-mobile-phone user-profile-icon"></i> Téléphone: 21331A2312 </li>
+                              </ul>
+                            </div>
+                            
+                          </div>
+                          <div class="col-xs-12 bottom text-center" >
+						  <div class="col-xs-12 col-sm-6 emphasis"></div>    
+                            <div class="col-xs-12 col-sm-6 emphasis" >
+                             
+                              <button type="button" class="btn btn-danger btn-xs" >
+                                <i class="fa fa-times"> </i> Annuler 
+                              </button>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      
+                      <div class="col-md-4 col-sm-4 col-xs-12 profile_details">
+                        <div class="well profile_view">
+                          <div class="col-sm-12">
+                            <h4 class="brief"><i>N° commande: 231</i></h4>
+                            <div class="left col-xs-7">
+                            
+                              <ul class="list-unstyled">
+                                <li>
+							    <i class="fa fa-calendar "></i>  Date:  24 mai  </li> 
+							    <li><i class="fa fa-clock-o "></i>  Horraire:  10H  </li>
+								
+								<li><i class="fa fa-map-marker"></i> Départ: San Francisco </li>
+								
+								<li><i class="fa fa-flag"></i> Destination: San Francisco</li>
+								<li><i class="fa fa-user"></i> Client: Samuel Doe </br></li>
+								<li><i class="fa fa-mobile-phone user-profile-icon"></i> Téléphone: 21331A2312 </li>
+                              </ul>
+                            </div>
+                            
+                          </div>
+                          <div class="col-xs-12 bottom text-center" >
+						  <div class="col-xs-12 col-sm-6 emphasis"></div>    
+                            <div class="col-xs-12 col-sm-6 emphasis" >
+                             
+                              <button type="button" class="btn btn-danger btn-xs" >
+                                <i class="fa fa-times"> </i> Annuler
+                              </button>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+
+                      
+
+                      
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+
+                    </div>
+
+                    
+
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        
+        <!-- /page content -->
+
+        <!-- footer content -->
+        <footer>
+         
+          <div class="clearfix"></div>
+        </footer>
+        <!-- /footer content -->
+      </div>
+    </div>
+
+    <!-- jQuery -->
+    <script src="../vendors/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap -->
+    <script src="../vendors/bootstrap/dist/js/bootstrap.min.js"></script>
+    <!-- FastClick -->
+    <script src="../vendors/fastclick/lib/fastclick.js"></script>
+    <!-- NProgress -->
+    <script src="../vendors/nprogress/nprogress.js"></script>
+    <!-- ECharts -->
+    <script src="../vendors/echarts/dist/echarts.min.js"></script>
+
+    <!-- Custom Theme Scripts -->
+    <script src="../build/js/custom.min.js"></script>
+
+  </body>
+</html>
