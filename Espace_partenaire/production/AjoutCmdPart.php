a6472cea9c69f31eb0ffbcd947a58a73365c50d4
wafa raboudi <wafaraboudi10@gmail.com>
2019-09-15 22:41:54 +0200

    FormCmd

---
+++ Espace_partenaire/production/AjoutCmdPart.php
@@ -0,0 +1,57 @@
+<?php 
+
+require_once 'db_connect.php';
+
+if($_POST) {
+	$Adresse_depart = $_POST['Adresse_depart'];
+	$Date = $_POST['Date'];
+	$Adresse_arrive = $_POST['Adresse_arrive'];
+	$Heure = $_POST['Heure'];
+	$type_voiture = $_POST['type_voiture'];
+	$nb_ouvruer = $_POST['nb_ouvruer'];
+	$nb_place_dispo = $_POST['nb_place_dispo'];
+	$n_facture = $_POST['n_facture'];
+	$Nom = $_POST['Nom'];
+	$Prenom = $_POST['Prenom'];
+	$telClient= $_POST['telClient'];
+	$Email = $_POST['Email'];
+	$id_partenaire=$_POST['id_partenaire'];
+	
+
+	$sql = "INSERT INTO client (Nom, Prenom, telClient, Email) VALUES ('$Nom', '$Prenom', '$telClient', '$Email')";
+	if($connect->query($sql) === TRUE) {
+		echo'<script language="javascript"> 
+	
+	alert("Successfully added");
+	</script>';
+	} else {
+		echo "Error " . $sql . ' ' . $connect->connect_error;
+	}
+	$sql1="SELECT id_client from client where telClient={$telClient} and Email={$Email}";
+	
+	$result=$connect->query($sql1);
+$lig=mysqli_fetch_array($result);
+$id=$lig[0];
+	
+	
+	//$sql2= "select max(n_cmd) from commende";
+	//$res=$connect->query($sql2);
+	//$r=$res->fetch_assoc();
+	//$n_cmd=$r[0]+1;
+	
+	
+	$sql3 = "INSERT INTO commende (Adresse_depart,Adresse_arrive,Date,Heure,type_voiture,nb_place_dispo,nb_ouvruer,typeClient,n_cmd,prix,n_facture,client_id_client,partenaire_id_partenaire,etatCmd)
+	VALUES ('$Adresse_depart', '$Adresse_arrive', '$Date', '$Heure','$type_voiture','$nb_place_dispo','$nb_ouvruer','partenaire',13,12,'$n_facture',4,'$id_partenaire',0)";
+	if($connect->query($sql3) === TRUE) {
+		echo'<script language="javascript"> 
+	
+	window.location="formCmdPart.php";
+	</script>';
+	} else {
+		echo "Error " . $sql3 . ' ' . $connect->connect_error;
+	}
+
+	$connect->close();
+}
+
+?>
--- Espace_partenaire/production/formCmdPart.php
+++ Espace_partenaire/production/formCmdPart.php
@@ -1,4 +1,10 @@
-<?php require_once 'db_connect.php'; ?>
+<?php require_once 'db_connect.php'; 
+$id_partenaire=1;
+
+$sql = "SELECT * FROM partenaire where id_partenaire={$id_partenaire}";
+$result = $connect->query($sql);
+$row = $result->fetch_assoc(); 
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -54,7 +60,7 @@
 					<li><a href="formCmdPart.php"> <i class="fa fa-edit"></i>Réservation</a></li>
                     <li><a href="listCMDPart.php"><i class="fa fa-list"></i>Liste en attente</a></li>
 					<li><a href="historiquePart.php"><i class="fa fa-clock-o"></i>Historique</a></li>
-					<li><a href="reclamationPart.html"><i class="fa fa-comments-o"></i> Réclamation </a></li>    
+					<li><a href="reclamationPart.php"><i class="fa fa-comments-o"></i> Réclamation </a></li>    
                 </ul>
               </div>
               
@@ -77,15 +83,15 @@
               </div>
 
               <ul class="nav navbar-nav navbar-right">
-                <li class="">
+               <li class="">
                   <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-                    <img src="images/.jpg" alt="">user name
+                    <img src="images/<?php echo $row['logo'] ?>.jpg" alt=""><?php echo $row['nom_ste'] ?>
                     <span class=" fa fa-angle-down"></span>
                   </a>
                   <ul class="dropdown-menu dropdown-usermenu pull-right">
                     <li><a href="profilePart.php"> Mon compte</a></li>
                     
-                   
+                    
                     <li><a href="login.php"><i class="fa fa-sign-out pull-right"></i> Déconnexion</a></li>
                   </ul>
                 </li>
@@ -120,58 +126,133 @@
                   <div class="x_content">
 
                     <!-- Tabs -->
+					
                     <div >
                       
                       <div align="center">
                         <h2 class="StepTitle">Choisissez la destination et la date de votre commande</h2>
-                        <form class="form-horizontal form-label-left" >
 
-                          
+                        <form action="AjoutCmdPart.php/create.php" method="post" >
 
-                          <div class="form-group" >
+                        <div class="form-row">
+
+                          <div class="col" >
                             
                             <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-							<input type="text" class="form-control has-feedback-left" id="inputSuccess2" placeholder="Départ">
+							 
+							<input type="text" class="form-control has-feedback-left" name="Adresse_depart" placeholder="Départ" value="<?php echo $row['adresse'] ?>">
 							<span class="fa fa-map-marker form-control-feedback left" aria-hidden="true"></span>
 							</div>
 
                           </div>
-                          <div class="form-group">
-                            
-                            <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-							<input type="text" class="form-control has-feedback-left" id="inputSuccess2" placeholder="Destination">
-							<span class="fa fa-flag form-control-feedback left" aria-hidden="true"></span>
-							</div>
-
-                          </div>
-						    <div class='col-sm-4'>
-                    
-						<div class="form-group">
-							<div class='input-group date' id='myDatepicker2'>
-								<span class="input-group-addon">
-								   <span class="glyphicon glyphicon-calendar"></span>
-								</span>
-								<input type='text' class="form-control" placeholder="Date" />
-								
-							</div>
-						</div>
-						<div class="form-group">
-							<div class='input-group date' id='myDatepicker3'>
-								<span class="input-group-addon">
-								   <span class="glyphicon glyphicon-time"></span>
-								</span>
-								<input type='text' class="form-control" placeholder="Horaire" />
-								
-							</div>
-						</div>
-						
-                </div>
+                          <div class="col">
 						  
+							<div class='class="col-md-6 col-sm-6 col-xs-12'>
+                    
+								<div class="form-group">
+									<div class='input-group date' id='myDatepicker2'>
+										<span class="input-group-addon">
+										   <span class="glyphicon glyphicon-calendar"></span>
+										</span>
+										<input type='text' name="Date" class="form-control" placeholder="Date" />
+										
+									</div>
+								</div>
+							</div>
+                          </div>
+						</div>
+						  
+						 <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
+							<input type="text" class="form-control has-feedback-left" name="Adresse_arrive" placeholder="Destination">
+							<span class="fa fa-flag form-control-feedback left" aria-hidden="true"></span>
+						</div>
+						  
+						<div class='class="col-md-6 col-sm-6 col-xs-12'>
+							<div class="form-group">
+								<div class='input-group date' id='myDatepicker3'>
+									<span class="input-group-addon">
+									   <span class="glyphicon glyphicon-time"></span>
+									</span>
+									<input type='text' class="form-control" name="Heure" placeholder="Horaire" />
+									
+								</div>
+							</div>
+						</div>
+						 </br> 
+						   <h2 class="StepTitle">Des informations supplémentaires</h2>
+                          <div class="form-row">
+                           <div class="col"> 
+								<div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
+									<input type="text" class="form-control has-feedback-left" name="type_voiture" placeholder="Type de voiture">
+									<span class="fa fa-car form-control-feedback left" aria-hidden="true"></span>
+								</div>
+							</div>
+							<div class="col">
+								<div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
+									<input type="text" class="form-control has-feedback-left" name="nb_ouvruer" placeholder="Nombre de main d'oeuvre">
+									<span class="fa fa-male form-control-feedback left" aria-hidden="true"></span>
+								</div>
+							</div>
+                          </div>
+						   <div class="form-row">
+                           <div class="col"> 
+								<div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
+									<input type="text" class="form-control has-feedback-left" name="nb_place_dispo" placeholder="Nombre d'accompagnant">
+									<span class="fa fa-users form-control-feedback left" aria-hidden="true"></span>
+								</div>
+							</div>
+							<div class="col">
+								<div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
+									<input type="text" class="form-control has-feedback-left" name="n_facture" placeholder="N° facture">
+									<span class="fa fa-file-text form-control-feedback left" aria-hidden="true"></span>
+								</div>
+							</div>
+                          </div>
+						  <h2 class="StepTitle">Information du client</h2>
+                          <div class="form-row">
+                           <div class="col"> 
+								<div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
+									<input type="text" class="form-control has-feedback-left" name="Nom" placeholder="Nom">
+									<span class="fa fa-user form-control-feedback left" aria-hidden="true"></span>
+								</div>
+							</div>
+							<div class="col">
+								<div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
+									<input type="text" class="form-control has-feedback-left" name="Prenom" placeholder="Prénom">
+									<span class="fa fa-user form-control-feedback left" aria-hidden="true"></span>
+								</div>
+							</div>
+                          </div>
+						  <div class="form-row">
+                           <div class="col"> 
+								<div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
+									<input type="text" class="form-control has-feedback-left"  name="telClient" placeholder="Téléphone">
+									<span class="fa fa-phone form-control-feedback left" aria-hidden="true"></span>
+								</div>
+							</div>
+							<div class="col"> 
+								<div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
+									<input type="email" class="form-control has-feedback-left"  name="Email" placeholder="Email">
+									<span class="fa fa-envelope form-control-feedback left" aria-hidden="true"></span>
+								</div>
+							</div>
+						</div>
+														<div class="form-group">
+								<input type="hidden" name="id_partenaire" value="<?php echo $id_partenaire ?>" />
+								</div>
 							
-						
+						 <div class="form-group">
+                        <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
+                          <a href="formCmdPart.php"><button class="btn btn-danger" type="button">Annuler</button>
+						  <button class="btn btn-warning" type="reset">Rénitialiser</button>
+                          <button type="submit" class="btn btn-success">Envoyer</button>
+                        </div>
+                      </div>
                           
 
                         </form>
+						
+						
                       </div>
                       
                      
