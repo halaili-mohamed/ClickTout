58f6c3306553e2e45031eaa41c60fdb897c598b0
halaili_mohamed <halalimohamed@hotmal.com>
2019-09-17 18:00:57 +0100

    integraation fun random in ajout_transporteur_action

--- administrateur/production/ajout_transporteur_action.php
+++ administrateur/production/ajout_transporteur_action.php
@@ -1,5 +1,17 @@
 <?php 
 require_once 'db_connect.php';
+
+function generateRandomString($length = 10) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
+
+
 if($_POST) {
 	$nom = $_POST['nom'];
 	$Prenom = $_POST['Prenom'];
@@ -8,8 +20,8 @@ if($_POST) {
 	$Adresse = $_POST['Adresse'];
 	$Matricule = $_POST['Matricule'];
 	$Type_Voiture = $_POST['Type_Voiture'];
-	$psudo = $_POST['psudo'];
-	$PWD = $_POST['PWD'];
+	$psudo = $_POST['nom'].'_'.$_POST['Prenom'].generateRandomString(5);
+	$PWD = generateRandomString();
 	
 	$sql = "INSERT INTO transporteur (nom, Prenom,Date_naiss,Email,Adresse,Matricule,Type_voiture,psudo,PWD) VALUES ('$nom', '$Prenom', '$Date_naiss','$Email','$Adresse','$Matricule','$Type_Voiture','$psudo','$PWD')";
 	if($connect->query($sql) === TRUE) {
