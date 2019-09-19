78f7ca38356b0d7866c3d64715cc2c4577463a7d
chellysahar <chellysahar36@gmail.com>
2019-09-19 12:44:03 +0200

    first migration

---
+++ loginPart.php
@@ -0,0 +1,29 @@
+
+<?php 
+ require_once 'connect.php';
+
+	if ($_POST) {
+	
+
+		$psudo=$_POST['psudo'];
+		$PWD=$_POST['PWD'];
+		
+		$query= "select * from partenaire where psudo='$psudo' AND PWD='$PWD'  ";
+		
+	    $result= mysqli_query($connect,$query);
+	  
+
+	    if (mysqli_num_rows($result) ==1) {
+	    	
+	    	header("location:conPar.php");
+	   
+	    }else
+	    {
+	    	
+	    	echo "<script type='text/javascript'>alert('hhh');</script>";
+	    }
+
+$connect->close();
+
+	}
+ ?>
---
+++ loginTransp.php
@@ -0,0 +1,28 @@
+
+<?php 
+ require_once 'connect.php';
+
+
+if ($_POST) {
+
+
+		$psudo1=$_POST['psudo1'];
+		$PWD1=$_POST['PWD1'];
+
+		
+		$query1= "select * from transporteur where psudo='$psudo1' AND PWD='$PWD1'  ";
+	    
+	    $result1= mysqli_query($connect,$query1);
+
+	    if (mysqli_num_rows($result1) ==1) {
+	    	
+	    	header("location:conPar1.php");
+	    }else
+	    {
+	    	echo "Error " . $query1 . ' ' . $connect->connect_error;
+
+	    }
+
+  $connect->close();
+	}
+ ?>
