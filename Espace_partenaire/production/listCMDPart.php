ee8bca3468c0249841ec794c01a427bd27e35707
wafa raboudi <wafaraboudi10@gmail.com>
2019-09-23 00:08:37 +0200

    jquery

--- Espace_partenaire/production/listCMDPart.php
+++ Espace_partenaire/production/listCMDPart.php
@@ -31,7 +31,7 @@ $row = $result->fetch_assoc();
     <link href="../build/css/custom.min.css" rel="stylesheet">
 	
 	<link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
-	<script src="https://code.jquery.com/jquery-3.3.1.js" ></script>
+	<script src="https://code.jquery.com/jquery-2.1.0.js" ></script>
 	<script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
 
 	<script >
