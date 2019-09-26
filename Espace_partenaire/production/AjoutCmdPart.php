c335e0f4a7775a16a4ddfd9b21f424971778a225
wafa raboudi <wafaraboudi10@gmail.com>
2019-09-26 20:11:35 +0200

    numCMD

--- Espace_partenaire/production/AjoutCmdPart.php
+++ Espace_partenaire/production/AjoutCmdPart.php
@@ -27,19 +27,7 @@ if($_POST) {
 	$id_client=$lig['id_client'];
 	
 	
-	$sql2= "select max(n_cmd) as nCMD from commende";
-	$res=$connect->query($sql2);
-	if($connect->query($sql2) === TRUE) {
-		echo  '<script language="javascript"> 
-	
-	alert("selected");
-	</script>';
-	} else {
-		echo "Error " . $sql2 . ' ' . $connect->connect_error;
-	}
-	$r=$res->fetch_assoc();
-	$n_cmd=$r['nCMD']+1;
-	
+	$n_cmd="c".$id_client;
 	
 	$sql3 = "INSERT INTO commende (Adresse_depart,Adresse_arrive,Date,Heure,type_voiture,nb_place_dispo,nb_ouvruer,typeClient,n_cmd,prix,n_facture,client_id_client,partenaire_id_partenaire,etatCmd)
 	VALUES ('$Adresse_depart', '$Adresse_arrive', '$Date', '$Heure','$type_voiture','$nb_place_dispo','$nb_ouvruer','partenaire','$n_cmd',12,'$n_facture','$id_client','$id_partenaire',0)";
